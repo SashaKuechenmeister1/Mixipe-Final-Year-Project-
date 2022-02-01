a519af2359afd61a2c28793358b82230631f6e27
unknown <c18404082@mytudublin.ie>
2022-02-01 19:19:24 +0000

    Added Demo Swiping cards

--- Mixipe/app/src/main/java/com/example/mixipe/MainActivity.java
+++ Mixipe/app/src/main/java/com/example/mixipe/MainActivity.java
@@ -2,24 +2,79 @@ package com.example.mixipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
+
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ArrayList<String > s;
+    ArrayAdapter arrayAdapter;
+    int n=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        s=new ArrayList<String >();
+        s.add("ONE");
+        s.add("TWO");
+        s.add("THREE");
+        s.add("FOUR");
+        s.add("FIVE");
+        s.add("SIX");
+        s.add("SEVEN");
+
+        SwipeFlingAdapterView swipeFlingAdapterView=(SwipeFlingAdapterView) findViewById(R.id.card);
+
+        arrayAdapter=new ArrayAdapter<String>(this,R.layout.swipeablecards,R.id.textView,s);
+        swipeFlingAdapterView.setAdapter(arrayAdapter);
+        swipeFlingAdapterView.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                s.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object o) {
+
+            }
+
+            @Override
+            public void onRightCardExit(Object o) {
+
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int i) {
+
+            }
+
+            @Override
+            public void onScroll(float v) {
+
+            }
+        });
     }
+
 
     public void logout (View view) {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(), Login.class));
         finish();
     }
+
 }
